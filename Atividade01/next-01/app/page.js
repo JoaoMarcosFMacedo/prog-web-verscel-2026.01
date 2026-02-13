fb3241857f5cb2dc62df074a97f384c118ed25fe
JoaoMarcosFMacedo <104098877+JoaoMarcosFMacedo@users.noreply.github.com>
2026-02-13 00:58:37 +0000

    Página finalizada

--- Atividade01/next-01/app/page.js
+++ Atividade01/next-01/app/page.js
@@ -1,11 +1,23 @@
 import Image from "next/image";
+import "./globals.css";
 
 
 export default function Pagina01 () {
   return (
+    <>
     <div>
       <h1>  Dor.  </h1>
-      <h2> Eu não sei expressar o quanto dói não te ter mais aqui <br/> O quanto dói lembrar as coisas boas e não poder repeti-las <br/>O quanto dói lembrar as ruins s sentir um buraco no peito <br/>O quanto dói renunciar o meu desejo <br/><br/> Eu não posso voltar atrás, não quando não tem nada nada para o que eu voltar<br/>Por que eu voltaria?<br/>Só existe um grande vazio oco entre nós agora<br/>Eu não quero voltar para o ponto onde paramos<br/> Porque foi ele que me fez ir embora<br/> E dói porque, apesar de ser vazio, ainda existe tanta coisa aqui dentro.<br/> Eu ainda espero por um sinal<br/><br/> Mas ela nunca veio nem virá <br/> Eu ainda espero por uma mudança, mas como alguém pode mudar que nem entende o que errou pode mudar?<br/> A dor e o amor andam lado a lado dentro de mim<br/> Como pode existir tanta verdades nos seus olhos<br/>E tanta mentira nas suas atitudes?<br/><br/> É tão contraditório, tão confuso<br/> não te querer aqui é como gritar em silêncio<br/>Não importa quanto tempo passe<br/> Não importa o quanto eu cuide da ferida<br/> Mil séculos se passarão<br/> E você continuará sendo a dor da minha vida.</h2> 
+      <h2> Eu não sei expressar o quanto dói não te ter mais aqui<br/>O quanto dói lembrar as coisas boas e não poder repeti-las<br/>O quanto dói lembrar as ruins e sentir um buraco no peito<br/>O quanto dói renunciar o meu desejo<br/><br/>
+      Eu não posso voltar atrás, não quando não tem nada para o que eu voltar<br/>Por que eu voltaria?<br/>Só existe um grande vazio oco entre nós agora<br/>Eu não quero voltar para o ponto onde paramos<br/> Porque foi ele que me fez ir embora<br/> E dói porque, apesar de ser vazio, ainda existe tanta coisa aqui dentro<br/><br/>
+      Eu ainda espero por um sinal<br/>Mas ela nunca veio nem virá <br/> Eu ainda espero por uma mudança, mas como alguém que nem entende o que errou pode mudar?<br/> A dor e o amor andam lado a lado dentro de mim<br/>Como pode existir tanta verdades nos seus olhos<br/>E tanta mentira nas suas atitudes?<br/><br/>
+      É tão contraditório, tão confuso<br/> Não te querer aqui é como gritar em silêncio<br/>Não importa quanto tempo passe<br/> Não importa o quanto eu cuide da ferida<br/> Mil séculos se passarão<br/> E você continuará sendo a dor da minha vida.</h2> 
     </div>
+      <Credito/>
+      </>
+    );
+  }
+  function Credito(){
+    return(
+      <div className="credt"><p> Se eu fosse capaz de amar, de Rebeca dantas </p></div>
     );
   }
